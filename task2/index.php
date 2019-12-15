52c605bdc8b318c3a4b89516226230bffa9ef2e0
Andrey Kleynos <goodmena@yandex.ru>
2019-12-16 00:25:26 +0300

    ДЗ2

--- task2/index.php
+++ task2/index.php
@@ -1,22 +1,14 @@
-<?php
-//1)
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 
-$n = 100;
-$array[]= [];
+</head>
+<body>
 
-for ($i = 0; $i < $n; $i++) {        //O(n)
-    for ($j = 1; $j < $n; $j *= 2) { //O(log(n))
-        $array[$i][$j]= true;
-    } }
-//O(n log(n))
-
-//2)
-
-$n = 100;
-$array[] = [];
-
-for ($i = 0; $i < $n; $i += 2) { //O(n/2)
-    for ($j = $i; $j < $n; $j++) { //O(n)
-        $array[$i][$j]= true;
-    } }
-//O(n^2)
+</body>
+</html>
